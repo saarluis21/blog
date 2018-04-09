b3a5d8c521a2407636d7d1a19236400fd0826994
luis salvador <saarluis199321@gmail.com>
2018-04-09 11:46:50 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return 'Login usuario';
+});
+
+Route::get('/logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('/catalog', function () {
+    return 'Listado películas';
+});
+
+Route::get('/catalog/show/{id}', function ($id) {
+    return 'Vista detalle película'.$id;
+});
+
+Route::get('/catalog/create', function () {
+    return 'Añadir película';
+});
+
+Route::get('/catalog/edit/{id}', function ($id) {
+    return 'Modificar película'.$id;
+});
